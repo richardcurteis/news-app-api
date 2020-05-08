4cb8a79cb9433c573d5589cb7b84601546d10fee
3therk1ll <richardcurteis@gmail.com>
2020-05-08 14:40:38 +0100

    created first app

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/etherk1ll/Development/python_projects/news-app-api/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
